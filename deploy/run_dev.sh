97bed2224b46197ab8d5108818e21fd3a6775571
flavio <flavio.sousa@semantix.com.br>
2020-03-24 02:38:11 -0300

    Initial commit

---
+++ deploy/run_dev.sh
@@ -0,0 +1,4 @@
+#python manage.py syncdb deprecated, use below command
+#python manage.py migrate #create or upgrade sqlite database
+#python manage.py startapp core
+python manage.py runserver
---
+++ restfulserver/restfulserver/core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html', {
+        'usuario': 'Fulano'
+    })
+
+
+def helloworld(request):
+    return HttpResponse('Hello World')
